569316b4e36789da74d46d309acf2d0d999f238b
DaviAieta <aieta.davi@gmail.com>
2024-01-29 00:48:03 -0300

    models

---
+++ src/models/condominium.models.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize')
+const db = require('./conn.models')
+
+const Condominios = db.define('condominios', {
+     id_condominio: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false
+     },
+     razao_social: {
+          type: Sequelize.STRING
+     },
+     logradouro: {
+          type: Sequelize.STRING
+     },
+     numend: {
+          type: Sequelize.STRING
+     },
+     complend: {
+          type: Sequelize.STRING
+     },
+     bairro: {
+          type: Sequelize.STRING
+     },
+     cidade: {
+          type: Sequelize.STRING
+     },
+     uf: {
+          type: Sequelize.STRING
+     },
+     cep: {
+          type: Sequelize.INTEGER
+     },
+     telefone: {
+          type: Sequelize.STRING
+     },
+     cnpj: {
+          type: Sequelize.STRING
+     },
+}, {
+     timestamps: false,
+}
+)
+
+Condominios.sync({ alter: true })
+     .then(() => {
+          console.log('Tabela Criada com sucesso')
+     })
+     .catch(erro => {
+          console.log('Erro ao criar a tabela: ' + erro)
+     })
+
+module.exports = Condominios
