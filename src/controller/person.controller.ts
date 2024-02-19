e88be6555eb08e2f41f7e2ed5bfa44e386d9fcbe
DaviAieta <aieta.davi@gmail.com>
2024-02-19 16:57:38 -0300

    Update person.controller | Create function as createPeson

--- src/controller/person.controller.ts
+++ src/controller/person.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import Person from '../models/person.models'
+import Unit from "../models/unit.models"
 
 export class PersonController{
     static async listPeople(req: Request, res: Response){
@@ -8,6 +9,23 @@ export class PersonController{
             return res.render('people/list', { people })
         } catch(error){
             return res.status(500).json({ error: error })
+        }
+    }
+    static async createPerson(req: Request, res: Response){
+        if(req.method == 'get'){
+            const units = await Unit.findAll()
+            return res.render('people/create', { units })
+        }
+        
+        else{
+            const {
+                nome, sobrenome, data_nascimento, cpf,
+                telefone, email, operador, administrador,
+                morador, super_admin, primeiro_acesso, token, data_cadastro,
+                id_unidade
+           } = req.body
+
+
         }
     }
 }
