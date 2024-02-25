e76a78ce851cc7cac456589b891d7661917ea490
DaviAieta <aieta.davi@gmail.com>
2024-02-25 20:41:16 -0300

    Create functions of mail.ts

--- src/services/mail.ts
+++ src/services/mail.ts
@@ -1,1 +1,33 @@
 require('dotenv').config()
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    secure: false,
+    auth: {
+        user: process.env.USER,
+        pass: process.env.MAILPASSORD
+    }
+})
+
+const sendMail = (from?: string, to?: string, subject?: string, html?: string) =>{
+    const mailOptions = {
+        from: process.env.MAILUSER,
+        to: to,
+        subject: subject,
+        html: html
+    }
+    // Debug
+    console.log(mailOptions)
+
+    return transporter.sendMail(mailOptions, function (error, info){
+        if(error){
+            return error
+        } else{
+            return console.log('Email sent !')
+        }
+    })
+}
+
+export default sendMail
