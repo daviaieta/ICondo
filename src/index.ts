99b6be3dfcfa8346908c838451d22b596bb4321b
DaviAieta <aieta.davi@gmail.com>
2024-04-16 18:11:40 -0300

    Update list Orders

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import condominiumRoutes from './routes/condominium.router'
 import unitRoutes from './routes/units.routes' 
 import personRouter from './routes/person.router'
 import authRouter from './routes/auth.routes'
+import orderRouter from './routes/order.routes'
 
 const app = express()
 configApp(app)
@@ -13,6 +14,7 @@ app.use('/condominiums', condominiumRoutes)
 app.use('/units', unitRoutes)
 app.use('/people', personRouter)
 app.use('/auth', authRouter)
+app.use('/orders', orderRouter)
 
 const PORT = 3000
 app.listen(PORT, () => {
